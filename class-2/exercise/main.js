314a421f44ed24680890842f29a9f7a20d9668bb
jDobkin <78820023+jDobkin@users.noreply.github.com>
2022-01-26 16:13:54 -0500

    commit

---
+++ class-2/exercise/main.js
@@ -0,0 +1,12 @@
+// from: https://docs.mapbox.com/help/tutorials/add-points-pt-3/
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/outdoors-v11",
+  center: [-75.15913247977207, 40.02727228000965],
+  zoom: 10.7,
+});
+
+
