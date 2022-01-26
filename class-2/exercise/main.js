2744be2ede9adb7d3c0b6a861a4fe1f8c29f05d1
jDobkin <78820023+jDobkin@users.noreply.github.com>
2022-01-26 17:09:22 -0500

    commit

--- class-2/exercise/main.js
+++ class-2/exercise/main.js
@@ -2,11 +2,27 @@
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
-const map = new mapboxgl.Map({
+const map = new mapboxgl.Map ({
   container: "map",
   style: "mapbox://styles/mapbox/outdoors-v11",
-  center: [-75.15913247977207, 40.02727228000965],
-  zoom: 10.7,
+  center: [-75.16362, 39.95238],
+  zoom: 9.5,
 });
 
+map.on("load", function(){
+  map.addSource("pev",{
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/dvrpc-municipal.json"
+  })
 
+  map.addLayer({
+    id: "pev",
+    type: "fill",
+    source: "SUM_CurPEV",
+    "source-layer": "pev",
+    paint: {
+      "fill-opacity": 0.5,
+      "fill-color": "green"
+    }
+  });
+});
